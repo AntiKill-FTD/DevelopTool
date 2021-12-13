ecdf1406541d377630997a86c0f7bb4273b74604
lschanga <lschanga@isoftstone.com>
2021-12-13 14:12:54 +0800

    更新Tool.Main目标框架

--- src/Tool.Main/FormMain.Designer.cs
+++ src/Tool.Main/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tool.Main
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(837, 459);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1215, 722);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
             this.ResumeLayout(false);
 
         }
---
+++ src/Tool.Main/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Tool.Main
+{
+    public static class StartUp
+    {
+        public static IServiceCollection services = new ServiceCollection();
+
+        //public static ServiceProvider Configuration()
+        //{
+        //    string DbPath = @"E:\GitRepository\GitHub\FTD\GitHubMine\DevelopTool\src\Tool.Test\Resource\Data\blogging.db";
+
+        //    //注入
+        //    //BloggingContext
+        //    return services.AddDbContext<BloggingContext>(option => option.UseSqlite($"Data Source={DbPath}"))
+        //                   .AddTransient<IBlogService, BlogService>()
+        //                   .BuildServiceProvider();
+        //}
+    }
+}
