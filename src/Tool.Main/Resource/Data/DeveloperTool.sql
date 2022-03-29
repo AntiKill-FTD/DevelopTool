02a39eff8d09a415ec5a296704a0f513d5fe64e5
lschanga <lschanga@isoftstone.com>
2022-03-29 11:08:56 +0800

    sqlserver表整理

--- src/Tool.Main/Resource/Data/DeveloperTool.sql
+++ src/Tool.Main/Resource/Data/DeveloperTool.sql
@@ -164,6 +164,9 @@ CREATE TABLE [dbo].[P_Menu](
 
 GO
 
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Ö÷¼ü' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_Menu', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'²Ëµ¥±àÂë' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_Menu', @level2type=N'COLUMN',@level2name=N'MenuCode'
 GO
 
