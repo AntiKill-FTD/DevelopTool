a8340f03761362fc9d1668f4ddbbd2ab496f596b
lschanga <lschanga@isoftstone.com>
2022-06-28 20:57:24 +0800

    新增菜单PDU数据导入

---
+++ src/Tool.Main/Forms/BusForms/PDUImport.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Tool.Main.Forms.BusForms
+{
+    partial class PDUImport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PDUImport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 633);
+            this.Name = "PDUImport";
+            this.Text = "PDUImport";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
